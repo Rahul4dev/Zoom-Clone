9537d4f79cc953dae051a893c725651e5b39257c
Rahul4dev <rahulsinghsignin@gmail.com>
2024-04-16 07:28:38 +0530

    Solved Error of SWR Config

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,34 +1,29 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import React from 'react';
+import { SignedIn, UserButton } from '@clerk/nextjs';
+
 import MobileNav from './MobileNav';
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  UserButton,
-} from '@clerk/clerk-react';
 
 const Navbar = () => {
   return (
-    <nav className="flex-between fixed w-full bg-dark-1 z-50 px-6 py-4 lg:px-10">
+    <nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
       <Link href="/" className="flex items-center gap-1">
         <Image
           src="/icons/logo.svg"
           width={32}
           height={32}
-          alt="Yoom logo"
+          alt="yoom logo"
           className="max-sm:size-10"
         />
         <p className="text-[26px] font-extrabold text-white max-sm:hidden">
-          Yoom
+          YOOM
         </p>
       </Link>
-
       <div className="flex-between gap-5">
         <SignedIn>
-          <UserButton />
+          <UserButton afterSignOutUrl="/sign-in" />
         </SignedIn>
+
         <MobileNav />
       </div>
     </nav>
